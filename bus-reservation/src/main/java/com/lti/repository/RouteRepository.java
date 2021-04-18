255dff31801535ce24c41e9f08c1967140346aeb
Raj <singhraj0206@yahoo.com>
2021-04-18 19:38:15 +0530

    Completed Route module Mihir Raj Shreya

--- bus-reservation/src/main/java/com/lti/repository/RouteRepository.java
+++ bus-reservation/src/main/java/com/lti/repository/RouteRepository.java
@@ -8,7 +8,7 @@ public class RouteRepository extends GenericRepository{
 	public boolean isRoutePresent(int busNumber) {
 		return (Long)
 				entityManager
-				.createQuery("select count(s.bus) from Route r where r.busNumber=:busNumber")
+				.createQuery("select count(r.bus) from Route r where r.bus.busNumber=:busNumber")
 				.setParameter("busNumber", busNumber)
 				.getSingleResult()==1?true:false;
 	}
