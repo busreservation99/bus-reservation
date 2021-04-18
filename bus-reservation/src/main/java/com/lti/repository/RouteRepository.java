3ec375906d2b0760e5a05492c41d169fb234a4c9
busreservation99 <bus.reservation99@gmail.com>
2021-04-18 19:49:24 +0530

    Route Module Completed- Raj Shreya Mihir

--- bus-reservation/src/main/java/com/lti/repository/RouteRepository.java
+++ bus-reservation/src/main/java/com/lti/repository/RouteRepository.java
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class RouteRepository extends GenericRepository{
 
-	public boolean isRoutePresent(int busNumber) {
+	public boolean isRoutePresent(int busNumber, String busStopName) {
 		return (Long)
 				entityManager
-				.createQuery("select count(r.bus) from Route r where r.bus.busNumber=:busNumber")
+				.createQuery("select count(r.bus) from Route r where r.bus.busNumber=:busNumber AND r.busStopName=:bstop")
 				.setParameter("busNumber", busNumber)
+				.setParameter("bstop", busStopName)
 				.getSingleResult()==1?true:false;
 	}
 }
